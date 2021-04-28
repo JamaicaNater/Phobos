c55ce23345f0c5c06f0510b71419d38e968800ee
JamaicaNater <52978102+JamaicaNater@users.noreply.github.com>
2021-04-28 00:38:35 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,11 +17,10 @@ public:
         while (str.length() != 0)
         {
             int substr_len = 4,
-                numerical_val;
+                decimal_value;
 
-            string length_four_binary,
-                   to_hex;
-
+            string binary_substring,
+                   append_to_hex;
 
             // Gets the first four characters of binary
             if (str.length() < 4)
@@ -30,27 +29,29 @@ public:
 
                 // if the string we read in is missing 3 character we need 3 zeros in front for it to be length
                 for (int i = 4 - substr_len; i > 0; i--)
-                    length_four_binary.push_back('0');
+                    binary_substring.push_back('0');
 
-                length_four_binary.append(str);
+                binary_substring.append(str);
             }
+            else
+                binary_substring = str.substr(0, 4);
 
-            to_hex = binToDec(length_four_binary)[0];
-            numerical_val = atoi(to_hex.c_str());
+            append_to_hex = binToDec(binary_substring);
+            decimal_value = atoi(append_to_hex.c_str());
 
-            if (numerical_val > 9)
+            if (decimal_value > 9)
             {
-                numerical_val -= 10;
-                to_hex = 'A';
+                decimal_value -= 10;
+                append_to_hex = 'A';
 
-                while ( numerical_val != 0 )
+                while (decimal_value != 0 )
                 {
-                    to_hex[0]++;
-                    numerical_val--;
+                    append_to_hex[0]++;
+                    decimal_value--;
                 }
             }
 
-            hex_value.append(to_hex);
+            hex_value.append(append_to_hex);
 
             str = str.substr(substr_len, str.length() - substr_len);
         }
@@ -61,14 +62,13 @@ public:
     string binToDec (string str)
     {
         int decimal = 0,
-            big_endian_pos;
+            big_endian_pos = 0;
 
-        for (int i = str.length()-1; i > 0; i--)
+        for (int i = str.length()-1; i >= 0; i--)
         {
-            big_endian_pos = str.length() - i - 1;
             char c = str[i];
-            //*************** REVIST
             decimal += (c - '0') * pow(2, big_endian_pos);
+            big_endian_pos++;
         }
         return to_string(decimal);
     }
@@ -91,9 +91,6 @@ public:
         rd = '$' + binToDec(instr.substr(16,5));
         shamt = binToDec(instr.substr(21,5));
         funct = binToHex(instr.substr(26, 5));
-
-
-
     }
 };
 
@@ -104,17 +101,27 @@ public:
         rt,
         imm;
 
+    bool is_branch = false,
+         is_load_store = false;
+
     I_Type( string instr, string op )
     {
- 
-        imm = binToHex(instr.substr(16,16));
-        
-        if ( false ) // imm refers to an address
-            imm = "0x" + imm;
+        string bin_opcode = instr.substr(0,6);
+        if (bin_opcode == "000100" || bin_opcode == "000101")
+            is_branch = true;
+
+        if (bin_opcode == "100100" || bin_opcode == "100000" || bin_opcode == "100101" || bin_opcode == "100001"
+            || bin_opcode == "100011" || bin_opcode == "101001" || bin_opcode == "101000" || bin_opcode == "101011")
+            is_load_store = true;
 
         opcode = op;
         rs = '$' + binToDec(instr.substr(6, 5));
         rt = '$' + binToDec(instr.substr(11, 5));
+
+        if (is_branch)
+            imm = binToHex(instr.substr(16,16));
+        else
+            imm = binToDec(instr.substr(16,16));
     }
 };
 
@@ -134,7 +141,12 @@ public:
     }
 };
 
+string generateOutputString(string);
 
+unordered_map<string, string> Rmap,
+        Imap,
+        Jmap,
+        Funcmap;
 
 int main() {
 /********************************************************Files*********************************************************/
@@ -172,18 +184,10 @@ int main() {
         return  4;
     }
 /********************************************************Files*********************************************************/
-    unordered_map<string, string> Rmap,
-                                  Imap,
-                                  Jmap,
-                                  Funcmap;
-    /*
-     *  Loop responsible for filling up the hash table, the loops reads in the
-     *  Opcode hash information from a file. Each pair of binary, opcode items
-     *  eg (100001, addu) is stored in a hash table corresponding to its R, I, J status.
-     */
+
+
     while (!OPCodein.eof())
     {
-
         char type;
         string opcode,
                operation;
@@ -219,13 +223,7 @@ int main() {
         Funcmap[opcode] = opname;
     }
 
-    /*
-     *  This is the loop where we iterate through the list of instructions. We get each line which is one instruction,
-     *  then  we isolate the first 6 bit to find the binary opcode. Later, we use this value to search the hashmaps
-     *  for a match in opcode.
-     *
-     *  If this is not found then the instrution is not valid.
-     */
+
     string binary_instruction;   // each line of instruction
     while (BINin >> binary_instruction)
     {
@@ -239,39 +237,57 @@ int main() {
             break;
         }
 
-        binary_opcode = binary_instruction.substr(0, 6); // Extract the first 6 characters of the line (opcode is first 6 bits)
-
-        // the .find() method returns the .end() iterator if the item is  not found in the list
-        // we check if the item is in the list
-        if (Rmap.find(binary_opcode) != Rmap.end())
-        {
-            R_Type temp = R_Type(binary_instruction, Rmap[binary_opcode]);
-
-            if (binary_opcode != "000000")
-                opcode = temp.opcode;
-            else
-                opcode = Funcmap[binary_instruction.substr(26, 6)];
-
-            if (temp.shamt != "0")
-                ASMout << opcode << " " <<  temp.rd << " " << temp.rs << " " << temp.rt << "(" << temp.shamt << ")" << endl;
-            else
-                ASMout << opcode << " " <<  temp.rd << " " << temp.rs << " " << temp.rt << endl;
-        }
-        else if (Imap.find(binary_opcode) != Imap.end())
-        {
-            I_Type temp = I_Type(binary_instruction, Imap[binary_opcode]);
-            ASMout << temp.opcode  << " " << temp.rs << " " << temp.rt << " " << temp.imm << endl;
-        }
-        else if (Jmap.find(binary_opcode) != Jmap.end())
-        {
-            J_Type temp = J_Type(binary_instruction, Jmap[binary_opcode]);
-            ASMout << temp.opcode << " " << temp.imm << endl;
-        }
-        else
-        {
-            cout << "Error Invalid";
-        }
+        ASMout << generateOutputString(binary_instruction);
     }
 
     return 0;
+}
+
+string generateOutputString(string bin_instr)
+{
+    string binary_opcode = bin_instr.substr(0, 6), // Extract the first 6 characters of the line (opcode is first 6 bits)
+           opcode,
+           output = "Invalid Opcode\n";
+
+    // the .find() method returns the .end() iterator if the item is  not found in the list
+    // we check if the item is in the list
+    if (Rmap.find(binary_opcode) != Rmap.end())
+    {
+        R_Type temp = R_Type(bin_instr, Rmap[binary_opcode]);
+
+        opcode = temp.opcode;
+        string bin_funct_code = bin_instr.substr(26, 6);
+
+        if (binary_opcode == "000000")
+            opcode = Funcmap[bin_funct_code];
+
+        if (temp.shamt == "0")
+            output = opcode + " " +  temp.rd + " " + temp.rs + " " + temp.rt + "\n";
+        else
+            output = opcode + " " + temp.rd + " " + temp.rs + " " + temp.shamt + "(" + temp.rt + ")\n";
+    }
+
+    if (Imap.find(binary_opcode) != Imap.end())
+    {
+        I_Type temp = I_Type(bin_instr, Imap[binary_opcode]);
+
+        output = temp.opcode  + " " + temp.rs + " " + temp.rt + " " + temp.imm + "\n";
+
+        if (temp.is_branch)
+            output = temp.opcode  + " " + temp.rs + " " + temp.rt + " 0x" + temp.imm + "\n";
+
+        if (temp.is_load_store)
+            if (atoi(temp.imm.c_str()) > 0)
+                output = temp.opcode  + " " + temp.rs + " (" +  temp.imm + ")" + temp.rt  + "\n";
+            else
+                output = temp.opcode  + " " + temp.rs + " " + temp.rt  + "\n";
+    }
+
+    if (Jmap.find(binary_opcode) != Jmap.end())
+    {
+        J_Type temp = J_Type(bin_instr, Jmap[binary_opcode]);
+        output + temp.opcode + " " + temp.imm + "\n";
+    }
+
+    return output;
 }
